2e3f9e77c54e0c16816f337dfd62cf689f5a6bcd
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2024-05-12 23:23:06 +0200

    About me section added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/logo.png";
+import logo3 from "../assets/logo3.png";
 import { FaGithub, FaInstagramSquare, FaLinkedin } from "react-icons/fa";
 //import { IoLogoCodepen } from "react-icons/io";
 
@@ -6,7 +6,7 @@ import { FaGithub, FaInstagramSquare, FaLinkedin } from "react-icons/fa";
 const Navbar = () => {
     return <nav className=" mb-20 flex items-center justify-between py-6">
         <div className="flex flex- items-center">
-            <img className="mx-2 w-20" src={logo} alt="" />
+            <img className="mx-2 w-20" src={logo3} alt="" />
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <FaLinkedin />
